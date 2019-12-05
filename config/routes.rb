266f0cf30cfda9c5c891789d977368da10d1f7ff
Zachary R. Hamlin <hamlinzach@gmail.com>
2019-12-04 20:55:26 -0600

    first push of weather app

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+   # the index page gets three routes:
+  root 'welcome#index'
+  get 'index' => 'welcome#index'
+  # "get" routes for when we initially come to the page
+  # (whether typing in manually, or coming to as the root)
+  post 'index' => 'welcome#index'  
+  # a "post route for when we come to the page" after submitting the form
+  get 'test' => 'welcome#test'
+
+end
